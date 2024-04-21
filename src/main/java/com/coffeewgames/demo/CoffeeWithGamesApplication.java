15965e44089fee20a75348a8b223c44f1e34857f
Rivaldo Ramos <ramosmendesfilho@hotmail.com>
2024-04-20 22:04:03 -0300

    Initial release

---
+++ src/main/java/com/coffeewgames/demo/CoffeeWithGamesApplication.java
@@ -0,0 +1,13 @@
+package com.coffeewgames.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeWithGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeWithGamesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/coffeewgames/demo/entities/User.java
@@ -0,0 +1,62 @@
+package com.coffeewgames.demo.entities;
+
+import java.io.Serializable;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Nonnull
+	private String name;
+
+	@Nonnull
+	private String email;
+
+	@Nonnull
+	private String password;
+
+	@Nonnull
+	private Integer wallet;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Integer getWallet() {
+		return wallet;
+	}
+
+}
